f950f3a7b3802349ecefd95ccc150ae31d380076
Nitish Kumar <kumar.nit1021@gmail.com>
2023-07-17 14:59:33 +0530

    Quiz backend

---
+++ Models/Quiz.Model.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const QuizSchema = mongoose.Schema({
+	creator: String,
+	title: String,
+	description: String,
+	questions: [
+		{
+			title: String,
+			answerOptions: [String],
+			correctOptions: Number,
+		},
+	],
+});
+
+const QuizModel = mongoose.model("Quiz", QuizSchema);
+
+module.exports = { QuizModel };
